8bd099c77005dd750a1406a9667a729079179e3f
usmanalyzz <95336022+usmanalyzz@users.noreply.github.com>
2024-11-01 11:24:16 +0500

    A new HTML is added for userflow page
    Uerflowdetail and UserflowAPI is added in the views

--- myPageflows/models.py
+++ myPageflows/models.py
@@ -7,6 +7,7 @@ class Product(models.Model):
     image = models.ImageField(upload_to='media/screenshots/')
     view_count = models.PositiveIntegerField(default=0)
     save_count = models.PositiveIntegerField(default=0)
+    category = models.CharField(max_length=50, null=True)
 
     def __str__(self):
         return self.title
@@ -17,8 +18,18 @@ class UserFlow(models.Model):
     category = models.CharField(max_length=100)
     video = models.FileField(upload_to='media/videos/')
     name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='media/screenshots/')
+    logo = models.ImageField(upload_to='media/logo/', null=True)
+    view_count = models.PositiveIntegerField(default=0)
     save_count = models.PositiveIntegerField(default=0)
     is_paid = models.BooleanField(default=True)
+
+    def save(self, *args, **kwargs):
+        if not self.view_count:
+            self.view_count = self.product.view_count
+        super().save(*args, **kwargs)
+
+
 
 
 
--- myPageflows/urls.py
+++ myPageflows/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
-from .views import IndexView, ProductView, CustomLoginView, CustomPasswordResetView, RegisterView, CustomLogoutView
+from .views import *
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
-    path('product/<int:product_id>/', ProductView.as_view(), name='product_detail'),
+    path('userflow/', Userflows.as_view(), name='userflow'),
     path('product/<str:title>/', IndexView.as_view(), name='product_title'),
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', CustomLogoutView.as_view(), name='logout'),
     path('forget/', CustomPasswordResetView.as_view(), name='forget'),
+    path('user-flows/<int:product_id>/', UserFlowDetail.as_view(), name='user_flow_detail'),
+    path('api/user-flow/<int:user_flow_id>/', UserFlowDetailAPI.as_view(), name='user_flow_api'),
+
 ]
